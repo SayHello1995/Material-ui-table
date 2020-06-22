4a3087410a314673dc1b1f64b91a8f51dd0b6f50
MaxKapralov <maksim.kapralau@finture.com>
2020-06-22 17:19:09 +0200

    fix

--- src/components/Table/EditableTable.js
+++ src/components/Table/EditableTable.js
@@ -8,55 +8,77 @@ import TableBody from '@material-ui/core/TableBody';
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 import IconButtonComponent from './IconButtonComponent';
+import Input from "@material-ui/core/Input";
+import renderIf from 'render-if';
 
 const PaperStyles = {
-    root: {
-        width: 800,
-        margin: "auto",
-        maxHeight: 500,
-        overflowY: "scroll"
-    }
-}
+  root: {
+    width: 800,
+    margin: "auto",
+    maxHeight: 500,
+    overflowY: "scroll"
+  }
+};
 
-const EditableTable = (props) => {
+class EditableTable extends React.PureComponent {
 
+  state = {
+    isEditable: false
+  };
+
+  changeEditState = () => {
+    this.setState((state) => ({isEditable: !state.isEditable}))
+  };
+
+  render() {
     return (
-        
-        <Paper style={PaperStyles.root}>
-                <div style={{display: "flex", justifyContent:"space-between"}}>
-                    <h2 >Weryfikacja odcinka pomiarowego 06610</h2>
-                    <Button startIcon={<AddIcon/>} color="primary" onClick={props.addRow}>Dodać wiersz</Button>
-                </div>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Actions</TableCell>
-                            {props.TableHeaders.map((header) => <TableCell>{header.name}</TableCell>)}
-                        </TableRow>   
-                    </TableHead>
-                    <TableBody>
-                        {props.Rows.map((row) => {
-                            return (
-                                <TableRow>
-                                    <TableCell>
-                                        {props.Actions.map((action) => {
-                                            return (
-                                                <IconButtonComponent icon={action.icon} onClick={action.onClick(row)} disabled={action.disabled}/>
-                                            )
-                                        })}
-                                    </TableCell>
-                                    {props.TableHeaders.map((h) => {
-                                        return(
-                                            <TableCell>{row[h.field]}</TableCell>      
-                                        )                                       
-                                    })}
-                                </TableRow>
-                            )
-                        })}
-                    </TableBody>
-                </Table>                
-            </Paper>
+      <Paper style={PaperStyles.root}>
+        <div style={{display: "flex", justifyContent: "space-between"}}>
+          <h2>Weryfikacja odcinka pomiarowego 06610</h2>
+          <Button startIcon={<AddIcon/>} color="primary" onClick={this.changeEditState}>Dodać wiersz</Button>
+        </div>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Actions</TableCell>
+              {this.props.TableHeaders.map((header) => <TableCell>{header.name}</TableCell>)}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.props.Rows.map((row) => {
+              return (
+                <TableRow>
+                  <TableCell>
+                    {this.props.Actions.map((action) => {
+                      return (
+                        <IconButtonComponent icon={action.icon} onClick={action.onClick(row)}
+                                             disabled={action.disabled}/>
+                      )
+                    })}
+                  </TableCell>
+                  {this.props.TableHeaders.map((h) => {
+                    return (
+                      <TableCell>{row[h.field]}</TableCell>
+                    )
+                  })}
+                </TableRow>
+              )
+            })}
+            <TableRow>
+              <TableCell>
+                Actions
+              </TableCell>
+              {renderIf(this.state.isEditable)(() => this.props.TableHeaders.map(header => (
+                <TableCell>
+                  {header.inputComponent}
+                </TableCell>
+              )))}
+            </TableRow>
+          </TableBody>
+        </Table>
+      </Paper>
     )
+  }
 }
 
 export default EditableTable;
--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -5,67 +5,86 @@ import CheckIcon from '@material-ui/icons/Check';
 import CloseIcon from '@material-ui/icons/Close';
 import EditableTable from './EditableTable';
 import Input from '@material-ui/core/Input'
+import Button from "@material-ui/core/Button";
 
 class TableHC extends React.Component {
 
-    state = {
-        rows: [],
-        actions: [],
-    }
+  state = {
+    rows: [],
+    actions: [],
+    newRow: {}
+  };
 
-    TableHeaders = [
-        {name: "godz.od", field: "timeFrom",},
-        {name: "godz.do", field: "timeTo", },
-        {name: "Kierunek", field: "direction", },
-        {name: "Status", field: "status", },
-        {name: "Komentarz", field: "comment", },
-    ]
+  inputChange = (fieldName) => (event) => {
+    const val = event.target.value;
 
-    TableActions = {
-        Delete: {onClick: (row) => () => this.deleteRow(row.id), icon: <DeleteIcon/> },
-        Edit: {onClick: (row) => () => this.editRow(row.id), icon: <EditIcon/>},
-        Confirm: {onClick: (row) => () => this.confirmRow(row.id), icon: <CheckIcon/>},
-        Close: {onClick: (row) => () => this.closeRow(row.id), icon: <CloseIcon/>}
-    }
+    this.setState((state) => ({...state, newRow: {...state.newRow, [fieldName]: val}}))
+  };
 
-    addRow = () => {
-        let NewRows = this.state.rows
-        let NewRowId = NewRows.length === 0 ? 1 : NewRows[NewRows.length - 1].id + 1
-        NewRows.push({id: NewRowId, timeFrom: '', timeTo:'', direction:'', status:'', comment:''}) 
-        this.setState({
-            rows: NewRows,
-            actions: [
-                this.TableActions.Confirm, 
-                this.TableActions.Close
-            ]
-        }) 
-    }
+  TableHeaders = [
+    {name: "godz.od", field: "timeFrom", inputComponent: <Input type="text" value={this.state.newRow.timeFrom} onChange={this.inputChange("timeFrom")}/>},
+    {name: "godz.do", field: "timeTo", inputComponent: <Input type="text" value={this.state.newRow.timeTo} onChange={this.inputChange("timeTo")}/>},
+    {name: "Kierunek", field: "direction", inputComponent: <Input type="text" value={this.state.newRow.direction} onChange={this.inputChange("direction")}/>},
+    {name: "Status", field: "status", inputComponent: <Input type="text" value={this.state.newRow.status} onChange={this.inputChange("status")}/>},
+    {name: "Komentarz", field: "comment", inputComponent: <Input type="text" value={this.state.newRow.comment} onChange={this.inputChange("comment")}/>},
+  ];
 
-    deleteRow = (id) => {
-        this.setState(state => ({...state, rows: state.rows.filter(r => r.id !== id)}))
-    }
+  TableActions = {
+    Delete: {onClick: (row) => () => this.deleteRow(row.id), icon: <DeleteIcon/>},
+    Edit: {onClick: (row) => () => this.editRow(row.id), icon: <EditIcon/>},
+    Confirm: {onClick: (row) => () => this.confirmRow(row.id), icon: <CheckIcon/>},
+    Close: {onClick: (row) => () => this.closeRow(row.id), icon: <CloseIcon/>}
+  };
 
-    editRow = (id) => {
-        let NewRows = this.state.rows
-        let foundRowIndex = NewRows.findIndex(row => row.id === id)
-        NewRows[foundRowIndex] = {id: id, timeFrom: <Input/>, timeTo:<Input/>, direction:<Input/>, status:<Input/>, comment:<Input/>}
-        this.setState({rows: NewRows})
-    }
+  addRow = () => {
+    let NewRows = this.state.rows;
+    let NewRowId = NewRows.length === 0 ? 1 : NewRows[NewRows.length - 1].id + 1;
+    NewRows.push({id: NewRowId, timeFrom: '', timeTo: '', direction: '', status: '', comment: ''});
+    this.setState({
+      rows: NewRows,
+      actions: [
+        this.TableActions.Confirm,
+        this.TableActions.Close
+      ]
+    })
+  };
 
-    confirmRow = (id) => {
-        console.log('confirmed')
-    }
+  deleteRow = (id) => {
+    this.setState(state => ({...state, rows: state.rows.filter(r => r.id !== id)}))
+  };
 
-    closeRow = (id) => {
-        console.log('closed')
-    }
+  editRow = (id) => {
+    let NewRows = this.state.rows;
+    let foundRowIndex = NewRows.findIndex(row => row.id === id);
+    NewRows[foundRowIndex] = {
+      id: id,
+      timeFrom: <Input/>,
+      timeTo: <Input/>,
+      direction: <Input/>,
+      status: <Input/>,
+      comment: <Input/>
+    };
+    this.setState({rows: NewRows})
+  };
 
-    render() {
+  confirmRow = (id) => {
+    console.log('confirmed')
+  };
 
-        return (
-            <EditableTable TableHeaders={this.TableHeaders} Actions={this.state.actions} Rows={this.state.rows} addRow={this.addRow}/>
-        )
-    }
+  closeRow = (id) => {
+    console.log('closed')
+  };
+
+  render() {
+
+    return (
+      <div>
+      <EditableTable TableHeaders={this.TableHeaders} Actions={this.state.actions} Rows={this.state.rows}
+                     addRow={this.addRow}/>
+        <Button onClick={() => console.log(this.state)}>Test</Button>
+      </div>
+    )
+  }
 }
 
 export default TableHC
